3565b8ca882f9ae346637d83655006c19ad612ab
Mateo Vidal <jvidalba@gmail.com>
2016-11-30 00:36:17 -0500

    Using classes with modules

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
-class Mortgage {
-
-    constructor(principal, years, rate) {
-        this.principal = principal;
-        this.years = years;
-        this.rate = rate;
-    }
-
-    get monthlyPayment() {
-        let monthlyRate = this.rate / 100 / 12;
-        return this.principal * monthlyRate / (1- (Math.pow(1/(1 + monthlyRate), this.years * 12)));
-    }
-
-    get amortization(){
-        let monthlyPayment = this.monthlyPayment;
-        let monthlyRate = this.rate / 100 / 12;
-        let balance = this.principal;
-        let amortization = [];
-
-        for (let y=0; y<this.years; y++) {
-            let interestY = 0;                                      // Interest payment for year y
-            let principalY = 0;                                     // Principal payment for year y
-            for (let m=0; m<12; m++) {
-                let interestM = balance * monthlyRate;              // Interest payment for month m
-                let principalM = monthlyPayment - interestM;        // Principal payment for month m
-                interestY = interestY + interestM;
-                principalY = principalY + principalM;
-                balance = balance - principalM;
-            }
-            amortization.push({principalY, interestY, balance});
-        }
-        return amortization;
-    }
-}
+import Mortgage from './mortgage2';
 
 document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
